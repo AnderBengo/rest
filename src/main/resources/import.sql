2d8b91befdc9cc56110461564cd6b8d18320bd12
Anderson Bengolea <anderson.bengolea.contractor@bbva.com>
2022-10-20 17:45:11 -0500

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tbl_cursos(id, curso) VALUES ( 1, 'INGLÉS' );
+INSERT INTO tbl_cursos(id, curso) VALUES ( 2, 'PROGRAMACIÓN WEB' );
+INSERT INTO tbl_cursos(id, curso) VALUES ( 3, 'ALGORITMOS' );
+
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 1, 'Pepe Rondan', 1 );
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 2, 'Rosa Queran', 2 );
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 3, 'Juan Jimenez', 2 );
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 4, 'Albert Dominguez', 3 );
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 5, 'Maria Morales', 3 );
+INSERT INTO tbl_estudiantes(id, nombres, curso_id) VALUES ( 6, 'Jose Risvan', 3 );
